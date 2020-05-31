74e15b714d933034b583ed280b796acff47e8aab
Elyahu <Elyahu41@gmail.com>
2020-05-31 17:02:35 -0400

    Should work fine, it was really annoying to work out the kinks

---
+++ WebScraper/src/Main.java
@@ -0,0 +1,112 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.*;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    static int EMAIL_MAX_COUNT = 10_000;
+    static int lastUploadCount = 0;
+    static int MAX_VISITS = 5;
+    static final Set<String> emails = Collections.synchronizedSet(new HashSet<>(10_000));
+    static final Set<String> emailsSentToDB = Collections.synchronizedSet(new HashSet<>(10_000));
+    static Set<String> linksToVisit = Collections.synchronizedSet(new HashSet<>(20_000));
+    static Set<String> linksFilter = Collections.synchronizedSet(new HashSet<>(20_000));
+    static Set<String> linksVisited = Collections.synchronizedSet(new HashSet<>(10_000));
+    static Map<String, Set<String>> maxLinksVisited = Collections.synchronizedMap(new HashMap<>());
+
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newFixedThreadPool(1500);
+        linksToVisit.add("https://www.touro.edu/");//start with touro.edu
+
+        while (!linksToVisit.isEmpty() && emails.size() <= EMAIL_MAX_COUNT) {
+            String link;
+            synchronized (linksToVisit) {
+                link = linksToVisit.stream().findFirst().get();
+                linksToVisit.remove(link);
+            }
+            if (hasTooManyVisits(link)) {
+                link = "";
+            }
+            if (!(link.equals(""))) {
+                linksVisited.add(link);
+                pool.execute(new WebScraper(link));
+            }
+
+            if (batchReadyForUpload() || lastUploadCount >= EMAIL_MAX_COUNT) {//upload in batches
+                ArrayList<String> upload = new ArrayList<>(emails);
+                upload.removeAll(emailsSentToDB);
+                upload.forEach(Main::dbUpload);
+                emailsSentToDB.addAll(upload);
+                lastUploadCount += 100;
+            }
+        }
+        pool.shutdownNow();
+    }
+
+    private static boolean hasTooManyVisits(String link) {
+        try {
+            URL currentURL = new URL(link);
+            String host = currentURL.getHost();
+
+            int startIndex = 0;
+            int nextIndex = host.indexOf('.');
+            int lastIndex = host.lastIndexOf('.');
+            while (nextIndex < lastIndex) {
+                startIndex = nextIndex + 1;
+                nextIndex = host.indexOf('.', startIndex);
+            }
+            synchronized (maxLinksVisited) {
+                if (startIndex > 0) {
+                    Set<String> tempSet = maxLinksVisited.get(host.substring(startIndex));
+                    if (tempSet == null) {
+                        tempSet = new HashSet<>();
+                        maxLinksVisited.put(host.substring(startIndex), tempSet);
+                    }
+                    tempSet.add(link);
+                    maxLinksVisited.put(host.substring(startIndex), tempSet);
+                    if (maxLinksVisited.get(host.substring(startIndex)).size() >= MAX_VISITS) {
+                        return true;
+                    }
+                } else {
+                    Set<String> tempSet = maxLinksVisited.get(host);
+                    if (tempSet == null) {
+                        tempSet = new HashSet<>();
+                        maxLinksVisited.put(host, tempSet);
+                    }
+                    tempSet.add(link);
+                    maxLinksVisited.put(host, tempSet);
+                    if (maxLinksVisited.get(host).size() >= MAX_VISITS) {
+                        return true;
+                    }
+                }
+            }
+        } catch (MalformedURLException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private static boolean batchReadyForUpload() {
+        final int BATCH_SIZE = 100;
+        return emails.size() - lastUploadCount > BATCH_SIZE;
+    }
+
+    public static void dbUpload(String email) {
+        String url = "database-1.cbjmpwcdjfmq.us-east-1.rds.amazonaws.com:1433"; // should pull from AWS Secrets Manager, environment variable, Properties class (key, value pairs)
+        String connectionUrl =
+                String.format("jdbc:sqlserver://%s;databaseName=jacobi;user=admin;password=mco368Touro", url);
+
+        try (Connection con = DriverManager.getConnection(connectionUrl); // Autoclosable
+             Statement stmt = con.createStatement()) {
+
+            String insertQuery = String.format("INSERT INTO Emails VALUES ('%s')", email);
+            stmt.executeUpdate(insertQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ WebScraper/src/WebScraper.java
@@ -0,0 +1,54 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebScraper implements Runnable {
+
+    String currentUrl;
+    String[] randomFileExtensions = {"png", "jpg", "gif", "pdf", "mp3", "css", "mp4", "mov", "7z", "zip", "mkv", "avi", "jpeg"};//common files
+
+    WebScraper(String url) {
+        this.currentUrl = url;
+        run();
+    }
+
+    @Override
+    public void run() {
+        scrape(currentUrl);
+    }
+
+    public void scrape(String url) {
+        try {
+            try { // double try block so the program doesn't stop on errors
+                Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                        .referrer("http://www.google.com").get();
+
+                Pattern emailPattern = Pattern.compile("[\\w\\d._]+@[\\w\\d]+\\.[\\w]{2,3}");
+                Matcher emailMatcher = emailPattern.matcher(doc.toString());
+
+                while (emailMatcher.find()) {//find and add emails
+                    String email = emailMatcher.group();
+                    if (Arrays.stream(randomFileExtensions).parallel().noneMatch(email::contains)) {//filter for any files that are not emails
+                        Main.emails.add(emailMatcher.group());
+                    }
+                }
+                synchronized (Main.linksFilter) {
+                    Main.linksFilter.addAll(doc.select("a[href]").eachAttr("abs:href"));//find and add all links on the page
+                    for (String randomFileExtension : randomFileExtensions) {
+                        Main.linksFilter.removeIf(s -> s.contains(randomFileExtension));//filter links for any files
+                    }
+                    synchronized (Main.linksToVisit) {
+                        Main.linksFilter.removeAll(Main.linksVisited);
+                        Main.linksToVisit.addAll(Main.linksFilter);
+                        Main.linksFilter.clear();
+                    }
+                }
+            } catch (IOException e) {
+            }
+        } catch (Exception | Error e) {
+        }
+    }
+}
